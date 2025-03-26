f4f41f2409f99b0e4e4cafe0cfb0687cb5865d15
Alejandro <aarevtri@myuax.com>
2025-03-26 19:01:14 +0100

    Project organization

--- custom_types.py
+++ custom_types.py
@@ -1,7 +1,19 @@
 from typing import Tuple
 
+# TODO: revisar en teso
 datos_vivienda = Tuple[
     "Habitaciones": int,
-    "Baños": int
-    # ""
+    "Baños": int,
+    "m2": int,
+    "Precio": int,
+    "Ubicación": str,
+    "Antigüedad": int,
+    "Calefacción": bool,
+    "Terraza": bool,
+    "Garaje": str,
+    "Trastero": bool,
+    "Piscina": bool,
+    "Orientacion": str,
+    "Cal. Energía": str,
+    "Url": str
     ]
