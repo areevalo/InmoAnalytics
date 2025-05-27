91e1c52145832b261c0594de7a1f9485372c4ab3
Alejandro <aarevtri@myuax.com>
2025-05-27 18:42:49 +0200

    increased sleep between properties verificated

--- inmoanalytics/management/commands/property_verification.py
+++ inmoanalytics/management/commands/property_verification.py
@@ -48,7 +48,7 @@ class Command(BaseCommand):
                         self.stderr.write(f"Could not open session for {property_obj.origin}")
                         continue
                     session_data['session'] = session
-                    time.sleep(5)
+                    time.sleep(6)
                 session_data['count'] += 1
 
                 response = session_data['session'].get(
@@ -110,7 +110,7 @@ class Command(BaseCommand):
                     else:
                         property_obj.save()
                         self.stdout.write(f"Property {property_obj.id} active (no changes)")
-                time.sleep(2 + 1 * random.random())  # To avoid hitting the server too hard
+                time.sleep(2.5 + 1 * random.random())  # To avoid hitting the server too hard
             except IntegrityError as e:
                 self.stderr.write(f"Error with {getattr(property_obj, 'url', 'unknown')}: checksum already saved: {e}")
                 property_obj.active = False
